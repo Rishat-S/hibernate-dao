f449c64183a15cff839c50e058243f10ba3b9c0a
Rishat Suleymanov <spk_irbis@mail.ru>
2021-08-30 12:00:40 +0300

    PersonCrudRepository implemented

--- src/main/java/ru/netology/hibernatedemo/repository/PersonCrudRepository.java
+++ src/main/java/ru/netology/hibernatedemo/repository/PersonCrudRepository.java
@@ -1,9 +1,13 @@
 package ru.netology.hibernatedemo.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.netology.hibernatedemo.entity.Person;
 
+import java.util.List;
+
+@Repository
 public interface PersonCrudRepository extends CrudRepository<Person, Long> {
 
-
+    List<Person> findByCityOfLiving(String city);
 }
--- src/main/java/ru/netology/hibernatedemo/service/PersonService.java
+++ src/main/java/ru/netology/hibernatedemo/service/PersonService.java
@@ -3,18 +3,17 @@ package ru.netology.hibernatedemo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.netology.hibernatedemo.entity.Person;
-import ru.netology.hibernatedemo.repository.PersonRepository;
+import ru.netology.hibernatedemo.repository.PersonCrudRepository;
 
 import java.util.List;
 
 @Service
 public class PersonService {
     @Autowired
-    PersonRepository personRepository;
-
+    PersonCrudRepository personCrudRepository;
 
     public List<Person> getPersonsByCity(String city) {
-        return personRepository.getPersonsByCity(city);
+        return personCrudRepository.findByCityOfLiving(city);
     }
 }
 
